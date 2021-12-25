62578538d6ca222137d008a1008c3af2665cb2a9
Willy Fajar Ramadhan <willyfr88@gmail.com>
2021-12-25 13:11:44 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'crimson': ['Crimson Pro', 'serif'],
+        'lora': ['Lora', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
